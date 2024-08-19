90a6105e16bd26a03bb8ccdf45a5bd7d653e2739
Nikita Berezhko <berezhko.nik16@gmail.com>
2024-08-19 10:22:18 +0300

    for last commit

--- Tests/Services/ReportServiceTests/GetByIdCasesTests.cs
+++ Tests/Services/ReportServiceTests/GetByIdCasesTests.cs
@@ -1,6 +1,48 @@
+using AutoMapper;
+using Domain;
+using Moq;
+using Services.Models.Request;
+using Services.RefitClients.Interfaces;
+using Services.Repositories.Interfaces;
+using Services.Services.Implementations;
+using Services.Validation;
+using Xunit;
+
 namespace Tests.Services.ReportServiceTests;
 
 public class GetByIdCasesTests
 {
-    
+    [Fact]
+    public async Task GetByIdCase_MustReturnReportModelWithRightId()
+    {
+        // Arrange
+        var service = CreateReportServiceForGetById(out Guid expected);
+        var model = new GetReportByIdModel
+        {
+            Id = expected
+        };
+        
+        // Act
+        var actual = await service.GetById(model);
+        
+        // Assert
+        Assert.Equal(expected, actual.Id);
+    }
+
+    private ReportService CreateReportServiceForGetById(out Guid expectedId)
+    {
+        expectedId = Guid.NewGuid();
+        var repository = new Mock<IReportRepository>();
+        repository.Setup(x => x.GetById(It.IsAny<Report>())).ReturnsAsync(new Report
+        {
+            Id = expectedId
+        });
+        
+        var service = new ReportService(repository.Object, 
+            Provider.Get<IMapper>(),
+            new Mock<IOrderApiClient>().Object,
+            Provider.Get<ReportValidator>());
+
+        return service;
+    }
 }
