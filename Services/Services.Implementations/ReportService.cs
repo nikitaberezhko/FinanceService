6df0f9b80077e6947543f2f77bcf5270f78d5fe6
Nikita Berezhko <berezhko.nik16@gmail.com>
2024-09-06 18:44:23 +0300

    add lock object to CreateReport method (2)

--- Services/Services.Implementations/ReportService.cs
+++ Services/Services.Implementations/ReportService.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using AutoMapper;
 using OrderService.Contracts.ApiModels;
 using Services.Models.OtherModels;
@@ -17,8 +18,60 @@ public class ReportService(
     : BaseReportService(reportRepository, mapper, orderApiClient, validator), 
         IReportService
 {
+    private object _lock = new();
+
+    #region OnlyLinq
+
+    // protected override ReportModel CreateReport(DateOnly date, List<OrderApiFullModel> orders)
+    // {
+    //     Stopwatch sw = new Stopwatch();
+    //     sw.Start();
+    //     var report = new ReportModel
+    //     {
+    //         Date = date,
+    //         Revenues = orders
+    //             .AsParallel().Select(x => new RevenueModel
+    //             {
+    //                 Id = Guid.NewGuid(),
+    //                 Amount = x.Price,
+    //                 OrderId = x.Id
+    //             }).ToList(),
+    //         Costs = orders
+    //             .AsParallel()
+    //             .Where(x => x.Costs > 0)
+    //             .Select(x => new CostModel
+    //             {
+    //                 Id = Guid.NewGuid(),
+    //                 Amount = x.Costs,
+    //                 OrderId = x.Id
+    //             }).ToList(),
+    //         TotalRevenue = orders.Sum(x => x.Price),
+    //         TotalCost = orders.Sum(x => x.Costs)
+    //     };
+    //
+    //     sw.Stop();
+    //     Console.WriteLine($"ReportService: {sw.ElapsedMilliseconds}");
+    //     return report;
+    // }
+
+    #endregion
+    
     protected override ReportModel CreateReport(DateOnly date, List<OrderApiFullModel> orders)
     {
+        Stopwatch sw = new Stopwatch();
+        sw.Start();
+        
+        double totalRevenue = 0;
+        double totalCost = 0;
+    
+        
+        Thread thread1 = new Thread(SummarizeRevenues);
+        Thread thread2 = new Thread(SummarizeCosts);
+        thread1.Start();
+        thread2.Start();
+        thread1.Join();
+        thread2.Join();
+        
         var report = new ReportModel
         {
             Date = date,
@@ -38,10 +91,34 @@ public class ReportService(
                     Amount = x.Costs,
                     OrderId = x.Id
                 }).ToList(),
-            TotalRevenue = orders.Sum(x => x.Price),
-            TotalCost = orders.Sum(x => x.Costs)
+            TotalRevenue = totalRevenue,
+            TotalCost = totalCost
         };
-
+        sw.Stop();
+        Console.WriteLine($"ReportService: {sw.ElapsedMilliseconds}");
+        
         return report;
+    
+        void SummarizeRevenues()
+        {
+            lock (_lock)
+            {
+                foreach (var order in orders)
+                {
+                    totalRevenue += order.Price;
+                }
+            }
+        }
+        
+        void SummarizeCosts()
+        {
+            lock (_lock)
+            {
+                foreach (var order in orders)
+                {
+                    totalCost += order.Costs;
+                }
+            }
+        }
     }
 }
