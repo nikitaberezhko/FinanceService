080602667680f04d44a9d6e86160e2f1b9e7d560
Nikita Berezhko <berezhko.nik16@gmail.com>
2024-09-16 11:35:44 +0300

    add serilog, add not stable realization to #region in RepostService.cs

--- Services/Services.Implementations/ReportService.cs
+++ Services/Services.Implementations/ReportService.cs
@@ -14,69 +14,18 @@ public class ReportService(
     IReportRepository reportRepository,
     IMapper mapper,
     IOrderApiClient orderApiClient,
-    ReportValidator validator) 
-    : BaseReportService(reportRepository, mapper, orderApiClient, validator), 
+    ReportValidator validator)
+    : BaseReportService(reportRepository, mapper, orderApiClient, validator),
         IReportService
 {
-    private object _lock = new();
-
-    #region OnlyLinq
-
-    // protected override ReportModel CreateReport(DateOnly date, List<OrderApiFullModel> orders)
-    // {
-    //     Stopwatch sw = new Stopwatch();
-    //     sw.Start();
-    //     var report = new ReportModel
-    //     {
-    //         Date = date,
-    //         Revenues = orders
-    //             .AsParallel().Select(x => new RevenueModel
-    //             {
-    //                 Id = Guid.NewGuid(),
-    //                 Amount = x.Price,
-    //                 OrderId = x.Id
-    //             }).ToList(),
-    //         Costs = orders
-    //             .AsParallel()
-    //             .Where(x => x.Costs > 0)
-    //             .Select(x => new CostModel
-    //             {
-    //                 Id = Guid.NewGuid(),
-    //                 Amount = x.Costs,
-    //                 OrderId = x.Id
-    //             }).ToList(),
-    //         TotalRevenue = orders.Sum(x => x.Price),
-    //         TotalCost = orders.Sum(x => x.Costs)
-    //     };
-    //
-    //     sw.Stop();
-    //     Console.WriteLine($"ReportService: {sw.ElapsedMilliseconds}");
-    //     return report;
-    // }
-
-    #endregion
-    
     protected override ReportModel CreateReport(DateOnly date, List<OrderApiFullModel> orders)
     {
-        Stopwatch sw = new Stopwatch();
-        sw.Start();
-        
-        double totalRevenue = 0;
-        double totalCost = 0;
-    
-        
-        Thread thread1 = new Thread(SummarizeRevenues);
-        Thread thread2 = new Thread(SummarizeCosts);
-        thread1.Start();
-        thread2.Start();
-        thread1.Join();
-        thread2.Join();
-        
         var report = new ReportModel
         {
             Date = date,
             Revenues = orders
-                .AsParallel().Select(x => new RevenueModel
+                .AsParallel()
+                .Select(x => new RevenueModel
                 {
                     Id = Guid.NewGuid(),
                     Amount = x.Price,
@@ -91,34 +40,77 @@ public class ReportService(
                     Amount = x.Costs,
                     OrderId = x.Id
                 }).ToList(),
-            TotalRevenue = totalRevenue,
-            TotalCost = totalCost
+            TotalRevenue = orders.Sum(x => x.Price),
+            TotalCost = orders.Sum(x => x.Costs)
         };
-        sw.Stop();
-        Console.WriteLine($"ReportService: {sw.ElapsedMilliseconds}");
-        
-        return report;
     
-        void SummarizeRevenues()
-        {
-            lock (_lock)
-            {
-                foreach (var order in orders)
-                {
-                    totalRevenue += order.Price;
-                }
-            }
-        }
-        
-        void SummarizeCosts()
-        {
-            lock (_lock)
-            {
-                foreach (var order in orders)
-                {
-                    totalCost += order.Costs;
-                }
-            }
-        }
+        return report;
     }
+
+    #region Realization on parallel (not stable)
+
+    // private object _lock = new();
+    
+    // protected override ReportModel CreateReport(DateOnly date, List<OrderApiFullModel> orders)
+    // {
+    //     Stopwatch sw = new();
+    //     sw.Start();
+    //     var report = new ReportModel
+    //     {
+    //         Date = date,
+    //         Revenues = new List<RevenueModel>(),
+    //         Costs = new List<CostModel>(),
+    //         TotalRevenue = 0,
+    //         TotalCost = 0
+    //     };
+    //     
+    //     Thread[] threads = new Thread[2];
+    //     threads[0] = new Thread(() => SummrizePart(orders.Take(orders.Count / 2).ToList()));
+    //     threads[1] = new Thread(() => SummrizePart(orders.Skip(orders.Count / 2).ToList()));
+    //
+    //     foreach (var t in threads) 
+    //         t.Start();
+    //     foreach (var t in threads) 
+    //         t.Join();
+    //
+    //     void SummrizePart(List<OrderApiFullModel> part)
+    //     {
+    //         for (int i = 0; i < part.Count; i++)
+    //         {
+    //             var revenue = new RevenueModel
+    //             {
+    //                 Amount = part[i].Price,
+    //                 OrderId = part[i].Id,
+    //                 Id = Guid.NewGuid()
+    //             };
+    //
+    //             CostModel? cost = null;
+    //             if (part[i].Costs > 0)
+    //             {
+    //                 cost = new CostModel
+    //                 {
+    //                     Amount = part[i].Costs,
+    //                     OrderId = part[i].Id,
+    //                     Id = Guid.NewGuid()
+    //                 };
+    //             }
+    //             
+    //             
+    //             lock (_lock)
+    //             {
+    //                 report.Revenues.Add(revenue);
+    //                 if (cost is not null) report.Costs.Add(cost);
+    //                 report.TotalRevenue += part[i].Price;
+    //                 report.TotalCost += part[i].Costs;
+    //             }
+    //         }
+    //     }
+    //
+    //     sw.Stop();
+    //     Console.WriteLine($"Report created in {sw.ElapsedTicks}ticks");
+    //     
+    //     return report;
+    // }
+
+    #endregion
 }
